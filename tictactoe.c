ed0c73ab5a32a43ab7ff79ddb8ea3a7ee727461b
shree <shreebandal0@gmail.com>
2021-03-31 21:25:05 +0530

    last Changes

---
+++ tictactoe.c
@@ -0,0 +1,345 @@
+#include<windows.h>
+void pattern1(char arr[]){
+printf("\t\t\t :-----: :-------::-------:\n");
+printf("\t\t\t :  %c  : :   %c   ::   %c   :\n",arr[0],arr[1],arr[2]);
+printf("\t\t\t :-----: :-------::-------:\n");
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:   %c   ::   %c   ::   %c   :\n",arr[3],arr[4],arr[5]);
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:   %c   ::   %c   ::   %c   :\n",arr[6],arr[7],arr[8]);
+printf("\t\t\t:-------::-------::-------:\n");
+}
+void pattern2(char arr[]){
+printf("\t\t\t:-------: :-----: :-------:\n");
+printf("\t\t\t:   %c   : :  %c  : :   %c   :\n",arr[0],arr[1],arr[2]);
+printf("\t\t\t:-------: :-----: :-------:\n");
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:   %c   ::   %c   ::   %c   :\n",arr[3],arr[4],arr[5]);
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:   %c   ::   %c   ::   %c   :\n",arr[6],arr[7],arr[8]);
+printf("\t\t\t:-------::-------::-------:\n");
+}
+void pattern3(char arr[]){
+printf("\t\t\t:-------::-------: :-----:\n");
+printf("\t\t\t:   %c   ::   %c   : :  %c  :\n",arr[0],arr[1],arr[2]);
+printf("\t\t\t:-------::-------: :-----:\n");
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:   %c   ::   %c   ::   %c   :\n",arr[3],arr[4],arr[5]);
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:   %c   ::   %c   ::   %c   :\n",arr[6],arr[7],arr[8]);
+printf("\t\t\t:-------::-------::-------:\n");
+}
+void pattern4(char arr[]){
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:   %c   ::   %c   ::   %c   :\n",arr[0],arr[1],arr[2]);
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t :-----: :-------::-------:\n");
+printf("\t\t\t :  %c  : :   %c   ::   %c   :\n",arr[3],arr[4],arr[5]);
+printf("\t\t\t :-----: :-------::-------:\n");
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:   %c  ::    %c   ::   %c   :\n",arr[6],arr[7],arr[8]);
+printf("\t\t\t:-------::-------::-------:\n");
+}
+void pattern5(char arr[]){
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:   %c   ::   %c   ::   %c   :\n",arr[0],arr[1],arr[2]);
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:-------: :-----: :-------:\n");
+printf("\t\t\t:   %c   : :  %c  : :   %c   :\n",arr[3],arr[4],arr[5]);
+printf("\t\t\t:-------: :-----: :-------:\n");
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:   %c   ::   %c   ::   %c   :\n",arr[6],arr[7],arr[8]);
+printf("\t\t\t:-------::-------::-------:\n");
+}
+void pattern6(char arr[]){
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:   %c   ::   %c   ::   %c   :\n",arr[0],arr[1],arr[2]);
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:-------::-------: :-----:\n");
+printf("\t\t\t:   %c   ::   %c   : :  %c  :\n",arr[3],arr[4],arr[5]);
+printf("\t\t\t:-------::-------: :-----:\n");
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:   %c   ::   %c   ::   %c   :\n",arr[6],arr[7],arr[8]);
+printf("\t\t\t:-------::-------::-------:\n");
+}
+void pattern7(char arr[]){
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:   %c   ::   %c   ::   %c   :\n",arr[0],arr[1],arr[2]);
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:   %c   ::   %c   ::   %c   :\n",arr[3],arr[4],arr[5]);
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t :-----: :-------::-------:\n");
+printf("\t\t\t :  %c  : :   %c   ::   %c   :\n",arr[6],arr[7],arr[8]);
+printf("\t\t\t :-----: :-------::-------:\n");
+}
+void pattern8(char arr[]){
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:   %c   ::   %c   ::   %c   :\n",arr[0],arr[1],arr[2]);
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:   %c   ::   %c   ::   %c   :\n",arr[3],arr[4],arr[5]);
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:-------: :-----: :-------:\n");
+printf("\t\t\t:   %c   : :  %c  : :   %c   :\n",arr[6],arr[7],arr[8]);
+printf("\t\t\t:-------: :-----: :-------:\n");
+}
+void pattern9(char arr[]){
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:   %c   ::   %c   ::   %c   :\n",arr[0],arr[1],arr[2]);
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:   %c   ::   %c   ::   %c   :\n",arr[3],arr[4],arr[5]);
+printf("\t\t\t:-------::-------::-------:\n");
+printf("\t\t\t:-------::-------: :-----:\n");
+printf("\t\t\t:   %c   ::   %c   : :  %c  :\n",arr[6],arr[7],arr[8]);
+printf("\t\t\t:-------::-------: :-----:\n");
+}
+void cross(){
+printf(":---:\t     \n");
+printf(": X :\t  O  \n");
+printf(":---:\t     \n");
+}
+void circle(){
+printf("     \t:---:\n");
+printf("  X  \t: O :\n");
+printf("     \t:---:\n");
+}
+void mainProcess(){
+int temp = 1,choice = 75,random;
+char arr[] = {' ',' ',' ',' ',' ',' ',' ',' ',' '},lastChoice;
+system("color e1");
+printf("Please Choose One\n\n");
+cross();
+while(1){
+char ch = _getch();
+if (ch == 0 || ch == -32)
+{
+    system("cls");
+    printf("Please Choose One\n\n");
+    ch = _getch();
+    choice = ch;
+    switch (ch)
+        {
+        case 75:
+            cross();
+            break;
+        case 77:
+            circle();
+            break;
+        default:
+            cross();
+            break;
+        }
+}
+else if(ch==13){
+system("cls");
+pattern1(arr);
+while(1){
+    char ch = _getch();
+    if (ch == 0 || ch == -32)
+    {
+        ch = _getch();
+        switch (ch)
+        {
+            case 72:
+                if(temp==4){
+                    system("cls");
+                    pattern1(arr);
+                    temp = 1;
+                }
+                else if(temp==5){
+                    system("cls");
+                    pattern2(arr);
+                    temp = 2;
+                }
+                else if(temp==6){
+                    system("cls");
+                    pattern3(arr);
+                    temp = 3;
+                }
+                else if(temp==7){
+                    system("cls");
+                    pattern4(arr);
+                    temp = 4;
+                }
+                else if(temp==8){
+                    system("cls");
+                    pattern5(arr);
+                    temp = 5;
+                }
+                else if(temp==9){
+                    system("cls");
+                    pattern6(arr);
+                    temp = 6;
+                }
+                break;
+            case 80:
+                if(temp==1){
+                    system("cls");
+                    pattern4(arr);
+                    temp = 4;
+                }
+                else if(temp==2){
+                    system("cls");
+                    pattern5(arr);
+                    temp = 5;
+                }
+                else if(temp==3){
+                    system("cls");
+                    pattern6(arr);
+                    temp = 6;
+                }
+                else if(temp==4){
+                    system("cls");
+                    pattern7(arr);
+                    temp = 7;
+                }
+                else if(temp==5){
+                    system("cls");
+                    pattern8(arr);
+                    temp = 8;
+                }
+                else if(temp==6){
+                    system("cls");
+                    pattern9(arr);
+                    temp = 9;
+                }
+                break;
+            case 75:
+                if(temp==2){
+                    system("cls");
+                    pattern1(arr);
+                    temp = 1;
+                }
+                else if(temp==3){
+                    system("cls");
+                    pattern2(arr);
+                    temp = 2;
+                }
+                else if(temp==5){
+                    system("cls");
+                    pattern4(arr);
+                    temp = 4;
+                }
+                else if(temp==6){
+                    system("cls");
+                    pattern5(arr);
+                    temp = 5;
+                }
+                else if(temp==8){
+                    system("cls");
+                    pattern7(arr);
+                    temp = 7;
+                }
+                else if(temp==9){
+                    system("cls");
+                    pattern8(arr);
+                    temp = 8;
+                }
+                break;
+            case 77:
+                if(temp==1){
+                    system("cls");
+                    pattern2(arr);
+                    temp = 2;
+                }
+                else if(temp==2){
+                    system("cls");
+                    pattern3(arr);
+                    temp = 3;
+                }
+                else if(temp==4){
+                    system("cls");
+                    pattern5(arr);
+                    temp = 5;
+                }
+                else if(temp==5){
+                    system("cls");
+                    pattern6(arr);
+                    temp = 6;
+                }
+                else if(temp==7){
+                    system("cls");
+                    pattern8(arr);
+                    temp = 8;
+                }
+                else if(temp==8){
+                    system("cls");
+                    pattern9(arr);
+                    temp = 9;
+                }
+                break;
+        }
+    }
+    else if(ch==13 && arr[temp-1]==' '){
+        if(choice==75)
+            arr[temp-1] = 'X';
+        else if(choice==77)
+            arr[temp-1] = 'O';
+        system("cls");
+        pattern1(arr);
+        if((arr[0] == 'X' && arr[1] == 'X' && arr[2] == 'X') || (arr[3] == 'X' && arr[4] == 'X' && arr[5] == 'X') || (arr[6] == 'X' && arr[7] == 'X' && arr[8] == 'X') ||
+           (arr[0] == 'X' && arr[3] == 'X' && arr[6] == 'X') || (arr[1] == 'X' && arr[4] == 'X' && arr[7] == 'X') || (arr[2] == 'X' && arr[5] == 'X' && arr[8] == 'X') ||
+           (arr[0] == 'X' && arr[4] == 'X' && arr[8] == 'X') || (arr[2] == 'X' && arr[4] == 'X' && arr[6] == 'X')){
+            if(choice==75)
+                printf("\n\t\t\tYou Won");
+            else if(choice==77)
+                printf("\n\t\t\tYou Loose");
+            printf("\n\n\t\t\tDo you wanna play again ?(Y/N)");
+            lastChoice = _getch();
+            if(lastChoice=='N' || lastChoice == 'n')
+                exit(0);
+            else{
+                system("cls");
+                mainProcess();
+            }
+           }
+        else if((arr[0] == 'O' && arr[1] == 'O' && arr[2] == 'O') || (arr[3] == 'O' && arr[4] == 'O' && arr[5] == 'O') || (arr[6] == 'O' && arr[7] == 'O' && arr[8] == 'O') ||
+           (arr[0] == 'O' && arr[3] == 'O' && arr[6] == 'O') || (arr[1] == 'O' && arr[4] == 'O' && arr[7] == 'O') || (arr[2] == 'O' && arr[5] == 'O' && arr[8] == 'O') ||
+           (arr[0] == 'O' && arr[4] == 'O' && arr[8] == 'O') || (arr[2] == 'O' && arr[4] == 'O' && arr[6] == 'O')){
+           if(choice==77)
+                printf("\n\t\t\tYou Won");
+            else if(choice==75)
+                printf("\n\t\t\tYou Loose");
+            printf("\n\n\t\t\tDo you wanna play again ?(Y/N)");
+            lastChoice = _getch();
+            if(lastChoice=='N' || lastChoice == 'n')
+                exit(0);
+            else{
+                system("cls");
+                mainProcess();
+
+            }
+           }
+        random = rand()%10;
+        if(arr[random]==' '){
+            if(choice==75)
+                arr[random] = 'O';
+            else if(choice==77)
+                arr[random] = 'X';
+        }
+        else{
+            while(1){
+                random = rand()%10;
+                if(arr[random]==' ')
+                    break;
+            }
+            if(choice==75)
+                arr[random] = 'O';
+            else if(choice==77)
+                arr[random] = 'X';
+        }
+        system("cls");
+        pattern1(arr);
+    }
+}
+}
+}
+}
+main(){
+mainProcess();
+}
